6a86e0e4e0050dab9aa76c49121191e6089b30d1
zachvalenta <zjv5002@gmail.com>
2018-04-28 18:37:00 -0400

    update db schema naming

--- polls/models.py
+++ polls/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Poll(models.Model):
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -12,18 +12,21 @@ class Poll(models.Model):
 
 
 class Choice(models.Model):
-    # what is the data type of `poll` attr? or does it just have to a Poll primary key (i.e. id)?
-    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=100)
+    # data type = Poll primary key?
+    # re: `related_name`, when using ORM API will be able to use `choices`
+    # instead of `choice_set`
+    poll_id = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
 
     def __str__(self):
         return self.choice_text
 
 
 class Vote(models.Model):
-    choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    choice_id = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
+    poll_id = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ("poll", "voted_by")
+        # meaning there can only be one record with this pairing of poll and user
+        unique_together = ("poll_id", "user_id")
