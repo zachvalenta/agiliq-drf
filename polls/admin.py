495efca46034a703149b6bd6764e5ba84dffe3cf
zachvalenta <zjv5002@gmail.com>
2018-05-03 13:22:17 -0400

    chapter 3: finish

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Poll, Choice
 
 admin.site.register(Poll)
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from polls.models import Poll
 
 
 def polls_list(request):
-    return HttpResponse("hi from polls_list")
+    polls = Poll.objects.all()
+    data = {"results": list(polls.values("text", "user_id__username", "pub_date"))}
+    return JsonResponse(data)
 
+# JsonResponse = HttpResponse w/ content-type = JSON
+# accessing related obj notation (`user_id__username` vs. `user_id.username`)
 
 def polls_detail(request, pk):
-    return HttpResponse('poll detail {}'.format(pk))
+    poll = get_object_or_404(Poll, pk=pk)
+    data = {"results": {
+        "text": poll.text,
+        "user_id": poll.user_id.username,
+        "pub_date": poll.pub_date
+    }}
+    return JsonResponse(data)
